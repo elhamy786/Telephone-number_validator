b2263f3e1cc895d91f2de920d30112780ce7b862
elhamy786 <elham.afzali1383@gmail.com>
2024-07-04 08:19:31 +0430

    solve script.js problems

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 document.getElementById('check-btn').addEventListener('click', () => {
-    const userInput = document.getElementById('user-input').value.trim();
-    const resultsDiv = document.getElementById('results-div');
-    const errorMessage = document.getElementById('error-message');
-    const validPatterns = [
-      /^1?\s?\d{3}-\d{3}-\d{4}$/,
-      /^1?\s?\(\d{3}\)\s?\d{3}-\d{4}$/,
-      /^1?\s?\d{10}$/,
-      /^1?\s?\(\d{3}\)\d{3}-\d{4}$/,
-      /^1?\s?\d{3}\s\d{3}\s\d{4}$/,
-    ];
-  
-    if (userInput === '') {
-      // eslint-disable-next-line no-alert
-      alert('Please provide a phone number');
-      errorMessage.textContent = '';  // Clear the error message div
-      return;
-    }
+  const userInput = document.getElementById('user-input').value.trim();
+  const resultsDiv = document.getElementById('results-div');
+  const errorMessage = document.getElementById('error-message');
+  const validPatterns = [
+    /^1?\s?\d{3}-\d{3}-\d{4}$/,
+    /^1?\s?\(\d{3}\)\s?\d{3}-\d{4}$/,
+    /^1?\s?\d{10}$/,
+    /^1?\s?\(\d{3}\)\d{3}-\d{4}$/,
+    /^1?\s?\d{3}\s\d{3}\s\d{4}$/,
+  ];
+  if (userInput === '') {
+    // eslint-disable-next-line no-alert
+    alert('Please provide a phone number');
+    errorMessage.textContent = ''; // Clear the error message div
+    return;
+  }
   
     const isValid = validPatterns.some((pattern) => pattern.test(userInput));
     resultsDiv.textContent = isValid ? `Valid US number: ${userInput}` : `Invalid US number: ${userInput}`;
