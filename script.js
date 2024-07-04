814e81876da2049d7783720fa8342c89e134540f
elhamy786 <elham.afzali1383@gmail.com>
2024-07-04 08:07:36 +0430

    Solve script.js linter errors

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-document.getElementById('check-btn').addEventListener('click', function() {
+document.getElementById('check-btn').addEventListener('click', () => {
     const userInput = document.getElementById('user-input').value.trim();
     const resultsDiv = document.getElementById('results-div');
     const validPatterns = [
-        /^1?\s?\d{3}-\d{3}-\d{4}$/,
-        /^1?\s?\(\d{3}\)\s?\d{3}-\d{4}$/,
-        /^1?\s?\d{10}$/,
-        /^1?\s?\(\d{3}\)\d{3}-\d{4}$/,
-        /^1?\s?\d{3}\s\d{3}\s\d{4}$/
+      /^1?\s?\d{3}-\d{3}-\d{4}$/,
+      /^1?\s?\(\d{3}\)\s?\d{3}-\d{4}$/,
+      /^1?\s?\d{10}$/,
+      /^1?\s?\(\d{3}\)\d{3}-\d{4}$/,
+      /^1?\s?\d{3}\s\d{3}\s\d{4}$/,
     ];
-
+  
     if (userInput === '') {
-        alert('Please provide a phone number');
-        return;
+      alert('Please provide a phone number');
+      return;
     }
-
-    let isValid = validPatterns.some(pattern => pattern.test(userInput));
+  
+    const isValid = validPatterns.some((pattern) => pattern.test(userInput));
     resultsDiv.textContent = isValid ? `Valid US number: ${userInput}` : `Invalid US number: ${userInput}`;
     resultsDiv.style.color = isValid ? 'green' : 'red';
-});
-
-document.getElementById('clear-btn').addEventListener('click', function() {
+  });
+  
+  document.getElementById('clear-btn').addEventListener('click', () => {
     document.getElementById('user-input').value = '';
     document.getElementById('results-div').textContent = '';
-});
+  });
