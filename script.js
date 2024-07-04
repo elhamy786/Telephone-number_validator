664ff5da33414d79430a8efe616f114da9957354
elhamy786 <elham.afzali1383@gmail.com>
2024-07-04 08:13:02 +0430

    Resolve script.js errors

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById('check-btn').addEventListener('click', () => {
     const userInput = document.getElementById('user-input').value.trim();
     const resultsDiv = document.getElementById('results-div');
+    const errorMessage = document.getElementById('error-message');
     const validPatterns = [
       /^1?\s?\d{3}-\d{3}-\d{4}$/,
       /^1?\s?\(\d{3}\)\s?\d{3}-\d{4}$/,
@@ -10,16 +11,20 @@ document.getElementById('check-btn').addEventListener('click', () => {
     ];
   
     if (userInput === '') {
+      // eslint-disable-next-line no-alert
       alert('Please provide a phone number');
+      errorMessage.textContent = '';  // Clear the error message div
       return;
     }
   
     const isValid = validPatterns.some((pattern) => pattern.test(userInput));
     resultsDiv.textContent = isValid ? `Valid US number: ${userInput}` : `Invalid US number: ${userInput}`;
     resultsDiv.style.color = isValid ? 'green' : 'red';
+    errorMessage.textContent = '';
   });
   
   document.getElementById('clear-btn').addEventListener('click', () => {
     document.getElementById('user-input').value = '';
     document.getElementById('results-div').textContent = '';
+    document.getElementById('error-message').textContent = '';
   });
